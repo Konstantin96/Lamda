61d66c90ea5384db45b0fd0983d368321e5f6538
Konstatin <34293528+Konstantin96@users.noreply.github.com>
2018-06-21 20:09:27 +0600

    Добавьте файлы проекта.

---
+++ Проект18/book.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Headers.h"
+
+class book {
+private:
+	string author;
+	string title;
+	int year;
+	int numOfCopies;
+public:
+	book() {
+		this->author = "";
+		this->title = "";
+		this->year = 0;
+		this->numOfCopies = 0;
+	}
+	book(string name, string bookname, int year, int count) {
+		this->author = name;
+		this->title = bookname;
+		this->year = year;
+		this->numOfCopies = count;
+
+	}
+
+	friend ostream& operator<<(ostream &os, book b) {
+		os << b.author << "\t" << b.title << "\t" << b.year << "\t" << b.numOfCopies << endl;
+		return os;
+	}
+
+	bool operator ==(size_t year) {
+		return(this->year == year);
+	}
+	string getAuthor()const {
+		return this->author;
+	}
+
+	int getYear()const {
+		return this->year;
+	}
+};
